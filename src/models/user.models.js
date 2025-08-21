f3cc84a0bf79087db1d849ee6b17ccebe783b60c
vedant-hx <vedant.bathwal10cdpsm@gmail.com>
2025-08-21 18:37:09 +0530

    added user and video model

---
+++ src/models/user.models.js
@@ -0,0 +1,87 @@
+import mongoose, { Schema, SchemaType } from "mongoose";
+import bcrypt from "bcrypt";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema(
+    {
+         username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+         },
+         email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+         },
+         fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+         },
+         avatar: {
+            type: String,
+            required: true
+         },
+         coverimage: {
+            type: String
+         },
+         watchhistory: [{
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+         }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshtoken: {
+
+        }
+
+    }, {timestamps: true} )
+
+
+    userSchema.pre("save", async function(next) {
+        if(!this.isModified("password")) return next
+        this.password = bcrypt.hash(this.password, 10)
+        next()
+    })
+
+    userSchema.methods.isPasswordCorrect = async function(password) 
+        {
+            return await bcrypt.compare(password, this.password)
+        
+    }
+
+    userSchema.methods.generateAccessToken = function() {
+        JsonWebTokenError.sign({
+            _id: this._id,
+            fullname: this.fullname,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRE
+        }
+    )
+    }
+
+    userSchema.methods.generateRefreshToken = function() {
+        JsonWebTokenError.sign({
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRE
+        }
+    )
+    }
+
+export const User = mongoose.model("User", userSchema)
---
+++ src/models/video.models.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+    {
+        videofile: {
+            type: String,
+            required: true
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duraton: {
+            type: Number,
+            required: true
+        },
+        view: {
+            type: Number,
+            default: 0
+        },
+        ispublished: {
+            type: Boolean,
+            deafult: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+},
+{
+    timestamps: true
+}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const video = mongoose.model("video", videoSchema)
